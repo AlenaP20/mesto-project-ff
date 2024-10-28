aae565afd876397b0a1b73ce42a57da361ea3c70
Alena Pechenitsyna <ad.pechenitsyna@yandex.ru>
2024-10-28 20:43:14 +0700

    добавлено появление и удаление карточек

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+`git@github.com:AlenaP20/mesto-project-ff.git` - ссылка на репозиторий
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(item) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__image').src = item.link;
+  cardElement.querySelector('.card__title').textContent = item.name;
+
+  deleteButton.addEventListener('click', deleteCard);
+  cardList.append(cardElement);
+};
 
 // @todo: Функция удаления карточки
+  function deleteCard(event) {
+    event.target.closest('.card').remove()
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(addCard);
