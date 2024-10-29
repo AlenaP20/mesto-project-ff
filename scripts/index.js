de705bd81ce4f513f85372cc584c28d5cc2749c1
Alena Pechenitsyna <ad.pechenitsyna@yandex.ru>
2024-10-29 18:12:20 +0700

    исправлена функция создания и добавления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,27 @@
 const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-const cardList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function addCard(item) {
+function createCard(cardData) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const deleteButton = cardElement.querySelector('.card__delete-button');
 
-  cardElement.querySelector('.card__image').src = item.link;
-  cardElement.querySelector('.card__title').textContent = item.name;
+  cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
+  cardElement.querySelector('.card__title').textContent = cardData.name;
 
   deleteButton.addEventListener('click', deleteCard);
-  cardList.append(cardElement);
+
+  return cardElement;
 };
+
+function addCard(cardData) {
+  const card = createCard(cardData);
+
+  cardsContainer.append(card);
+}
 
 // @todo: Функция удаления карточки
   function deleteCard(event) {
